25efc64d73cd4743b019fb78ac4921829aa3ba88
Vladimir Mozgovoy <47141140+vladimirmozgovoy@users.noreply.github.com>
2023-05-15 17:26:42 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -24,12 +24,13 @@ INSERT INTO `users` (`name`,`gender`,`birth_date`) VALUES ("Anastasia",2,UNIX_TI
 INSERT INTO `phone_numbers` (`user_id`,`phone`) VALUES (1,"4654564");
 INSERT INTO `phone_numbers` (`user_id`,`phone`) VALUES (1,"4654564");
 INSERT INTO `phone_numbers` (`user_id`,`phone`) VALUES (1,"4654564");
-WITH TEMP AS (SELECT users.id as user_id, users.name as name, phone as phone
-              FROM `users`
-                       INNER JOIN `phone_numbers` ON users.id = phone_numbers.user_id
-              where users.birth_date >= UNIX_TIMESTAMP(STR_TO_DATE('2001-01-01', '%Y-%m-%d'))
-                AND users.birth_date <= UNIX_TIMESTAMP(STR_TO_DATE('2005-12-31', '%Y-%m-%d'))
-              )
-SELECT name ,COUNT(phone) as count_phone FROM TEMP GROUP BY user_id;
+SELECT u.name, COUNT(p.phone) AS count_phoneFROM (
+    SELECT *
+    FROM users
+    WHERE gender = 2
+    AND birth_date BETWEEN UNIX_TIMESTAMP(:start_date) AND UNIX_TIMESTAMP(:end_date)
+) AS u
+LEFT JOIN phone_numbers AS p ON u.id = p.user_id
+GROUP BY u.id, u.name;
 
 
